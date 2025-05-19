82450c57c0ac0fab439beac129dbebd189dea33e
Kirill Svininykh <kirill.svininykh@gmail.com>
2025-05-20 00:03:35 +0300

    update build paths

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/simple_forest_editor' : ''
+		}
 	}
 }
 
